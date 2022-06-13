918b97990e22dd4f2ddf76ba8d1bd911e4ba6b6e
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-06-12 21:28:09 -0700

    update about page and build account page, added formik

--- bookr/src/pages/About.jsx
+++ bookr/src/pages/About.jsx
@@ -6,16 +6,17 @@ const About = () => {
   return (
     <>
       <Nav />
-      <Container display="flex" flexDirection="column" justifyContent="center" alignItems="center" textAlign="center">
+      <Container display="flex" flexDirection="column" justifyContent="center" alignItems="center" textAlign="center" w="100%" border='1px' borderColor='gray.600'>
         <Heading size="xl" mb={4} >About this app</Heading>
-        <Text m={2}>Do you like books?</Text>
-        <Text m={2}>Do you like keeping track of your books?</Text>
-        <Text m={2}>Do you like the idea of GoodReads but don't care at all about the social network part?</Text>
-        <Text m={6}>Me too.</Text>
-        <Text m={2}>Welcome to Bookr - a simple, straightforward way to keep track of your books</Text>
-        <Text m={2}>This app is made with Vite(React), ChakraUI, and the Google Books API</Text>
-        <Text m={2}>You can see it on Github <Link href="https://github.com/lenniecottrell/bookr" color="blue.500" textDecoration="underline">here</Link></Text>
-        <Text m={2} >If you like it, or have feedback, or have nothing better to do, feel free to let me know on <Link href="https://twitter.com/lenniecottrell" color="blue.500" textDecoration="underline">Twitter</Link> </Text>
+        <Text fontSize="xl" m={0}>Do you like books?</Text>
+        <Text fontSize="xl" m={0}>Do you like keeping track of your books?</Text>
+        <Text fontSize="xl" m={0}>Do you like the idea of GoodReads but don't care at all about the social network part?</Text>
+      </Container>
+      <Container  display="flex" flexDirection="column" justifyContent="center" alignItems="center" textAlign="center" mt="100"  border='1px' borderColor='gray.600'>
+        <Text fontSize="xl" m={2}>Welcome to Bookr - a simple, straightforward way to keep track of your books</Text>
+        <Text fontSize="xl" m={2}>This app is made with Vite (React), ChakraUI, and the Google Books API</Text>
+        <Text fontSize="xl" m={2}>You can see it on Github <Link href="https://github.com/lenniecottrell/bookr" color="blue.500" textDecoration="underline">here</Link></Text>
+        <Text fontSize="xl" m={2} >If you like it, or have feedback, or want to say hi, you can find me on <Link href="https://twitter.com/lenniecottrell" color="blue.500" textDecoration="underline">Twitter</Link> sometimes </Text>
       </Container>
     </>
   )
--- bookr/src/pages/Account.jsx
+++ bookr/src/pages/Account.jsx
@@ -1,21 +1,89 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from '../components/Nav'
 import {
   Container,
   Heading,
-  Text,
   FormControl,
   FormLabel,
   FormErrorMessage,
-  FormHelperText,
+  Input,
+  Button,
+  Text,
+  Link,
+  InputRightElement
 } from '@chakra-ui/react'
+import {Field, Form, Formik, useFormik } from 'formik'
 
 const Account = () => {
+  // come back to this when I'm working on auth again
+  // const [show, setShow] = useState(false)
+  // const handleClick= () => setShow(!show)
+
+
   return (
     <div>
       <Nav />
-      <Container display="flex" flexDirection="column" justifyContent="center" alignItems="center" textAlign="center">
-        <Heading size="xl" mb={4} >Log In</Heading>
+      <Heading size="xl" mb={4} textAlign="center">Log In</Heading>
+      <Container display="flex" flexDirection="column" justifyContent="center" alignItems="center" textAlign="center" mt={50} border='1px' borderColor='gray.400' borderRadius="10px" p="3rem">
+        <Formik
+          initialValues={{name: "", email: "", password: ""}}
+          onSubmit= {(values, actions) => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false)
+          }}
+        >
+          {/* there are a bunch of props in the Formik component--> https://formik.org/docs/api/formik */}
+          {(props) => (
+            <Form>
+              <Field name='name'>
+                {/* field and form are props of the Field component from formik */}
+                {({field, form}) => (
+                  <FormControl isInvalid={form.errors.name && form.touched.name}>
+                    <FormLabel htmlFor="name">Name</FormLabel>
+                    <Input {...field} id="name" placeholder="name" type="text"/>
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name='email'>
+                {/* field and form are props of the Field component from formik */}
+                {({field, form}) => (
+                  <FormControl isInvalid={form.errors.email && form.touched.email}>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input {...field} id="email" placeholder="email" type="email"/>
+                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name='password'>
+                {/* field and form are props of the Field component from formik */}
+                {({field, form}) => (
+                  <FormControl isInvalid={form.errors.password && form.touched.password}>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input {...field} id="password" placeholder="password" type="password"/>
+                    {/* <InputRightElement width="4.5rem">
+                      <Button h='1.75rem' size='sm' onClick={handleClick}>
+                        {show ? 'Hide' : 'Show'}
+                      </Button>
+                    </InputRightElement>  */}
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Button 
+                mt={4}
+                w="100%"
+                size="lg"
+                colorScheme='teal'
+                isLoading={props.isSubmitting}
+                type='submit'
+                >
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+        <Text>Need an account? <Link color="blue.400">Sign up</Link></Text>
       </Container>
     </div>
   )
