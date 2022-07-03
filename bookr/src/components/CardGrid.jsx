25d5f1a0d50ef997782c4d292746334388170c9f
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-07-03 08:01:42 -0700

    minor edits to cardgrid, commiting to push branches to master

--- bookr/src/components/CardGrid.jsx
+++ bookr/src/components/CardGrid.jsx
@@ -4,7 +4,7 @@ import BookCard from './BookCard'
 import axios from 'axios'
 
 const CardGrid = ({query}) => {
-  const [allBookData, setAllBookData] = useState({title: "hungry caterpillar", author: "richard scary?"})
+  const [allBookData, setAllBookData] = useState([])
   const [allBookTitles, setAllBookTitles] = useState([])
   const [allBookAuthors, setAllBookAuthors] = useState([])
   const [allBookImages, setAllBookImages] = useState([])
@@ -25,36 +25,36 @@ const CardGrid = ({query}) => {
     })
     .then((response) => {
       const allBooks = response.data.items
-      console.log(allBooks)
+      //console.log(allBooks)
       for (let i = 0; i < allBooks.length; i++) {
         books.push(allBooks[i].volumeInfo)
         titles.push(allBooks[i].volumeInfo.title)
         authors.push(allBooks[i].volumeInfo.authors)
       }
 
-      //authorList is an inner array of authors. If there's more than one, add a space to each item
-      for (let authorList of authors) {
-        if (authorList === undefined) {
-          authorList = [""]
-        }
-        if (authorList.length > 1) {
-          for (let i = 0; i < authorList.length - 1; i++) {
-            authorList[i] += ", "
-          }
-        }
-      }
+      // //authorList is an inner array of authors. If there's more than one, add a space to each item
+      // for (let authorList of authors) {
+      //   if (authorList === undefined) {
+      //     authorList = [""]
+      //   }
+      //   if (authorList.length > 1) {
+      //     for (let i = 0; i < authorList.length - 1; i++) {
+      //       authorList[i] += ", "
+      //     }
+      //   }
+      // }
       
-      for (let i = 0; i < allBooks.length; i++) {
-        if (allBooks[i].volumeInfo.imageLinks === undefined){
-          images.push({smallThumbnail: "No Image Available", thumbnail: "No Image Available"})
-          continue
-        }
-        images.push(allBooks[i].volumeInfo.imageLinks)
-      }
-      setAllBookData(books)
-      setAllBookTitles(titles)
-      setAllBookAuthors(authors)
-      setAllBookImages(images)
+      // for (let i = 0; i < allBooks.length; i++) {
+      //   if (allBooks[i].volumeInfo.imageLinks === undefined){
+      //     images.push({smallThumbnail: "No Image Available", thumbnail: "No Image Available"})
+      //     continue
+      //   }
+      //   images.push(allBooks[i].volumeInfo.imageLinks)
+      // }
+      setAllBookData(allBooks)
+      // setAllBookTitles(titles)
+      // setAllBookAuthors(authors)
+      // setAllBookImages(images)
     })
     .catch((err) => {
       console.error(err);
@@ -68,22 +68,51 @@ const CardGrid = ({query}) => {
       return
     }
     getBooks()
-  }, [query])
+    for (let book of allBookData) {
+        if (book.volumeInfo.imageLinks === undefined){
+          book.volumeInfo.imageLinks = {smallThumbnail: "No Image Available", thumbnail: "No Image Available"}
+        }
+        if (book.volumeInfo.authors === undefined) {
+          book.volumeInfo.authors = ""
+        }
+        if (book.volumeInfo.authors.length > 1) {
+          for (let i = 0; i < book.volumeInfo.authors.length - 1; i++) {
+          book.volumeInfo.authors[i] += ", "
+          }
+        }
+      }
+  }, [query, allBookData])
 
 
-  for (let i = 0; i < allBookData.length; i++) {
-    cards.push(
-      <SlideFade key={i} in={true}>
-        <GridItem className="gridItem" boxShadow="lg" w='100%' h='100%' bg='gray.100' borderRadius={6} >
-          <BookCard allBookData={allBookData[i]} title={allBookData[i].title} authors={allBookData[i].authors} image={allBookImages[i]}/>
-        </GridItem>
-      </SlideFade>
-    )
-  }
+  // for (let i = 0; i < allBookData.length; i++) {
+  //   cards.push(
+  //     <SlideFade key={i} in={true}>
+  //       <GridItem className="gridItem" boxShadow="lg" w='100%' h='100%' bg='gray.100' borderRadius={6} >
+  //         <BookCard allBookData={allBookData[i]} title={allBookData[i].title} authors={allBookData[i].authors} image={allBookImages[i]}/>
+  //       </GridItem>
+  //     </SlideFade>
+  //   )
+  // }
 
   return (
       <SimpleGrid minChildWidth={300} spacing={6} m="2rem">
-        {cards}
+        {allBookData.map((book) => {
+        <SlideFade key={book.id} in={true}>
+          <GridItem className="gridItem" boxShadow="lg" w='100%' h='100%' bg='gray.100' borderRadius={6} >
+            <BookCard 
+              bookData={book}
+              title={book.volumeInfo.title}
+              authors={JSON.stringify(book.volumeInfo.authors)}
+              image={JSON.stringify(book.volumeInfo.imageLinks.thumbNail)}
+            />
+          </GridItem>
+        </SlideFade>
+        })
+
+        }
+
+
+
       </SimpleGrid>
   )
 }
