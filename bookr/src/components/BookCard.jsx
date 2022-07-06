beaaf4a7b79d0bf6e73f36e77c98d423eed72436
lenniecottrell <64614859+lenniecottrell@users.noreply.github.com>
2022-07-06 05:09:39 -0700

    try to fix undefined images

--- bookr/src/components/BookCard.jsx
+++ bookr/src/components/BookCard.jsx
@@ -6,7 +6,7 @@ const BookCard = ({title, authors, image}) => {
   return (
     <Center>
       <Box className='bookCard' pt='1rem' display="flex" flexDirection="column" gap={3} w="100%">
-        <Image boxSize="250px" objectFit="contain" src={image.thumbnail} alt={image.thumbnail} alignSelf="center"/>
+        <Image boxSize="250px" objectFit="contain" src={image.thumbnail === undefined ? "" : `${image.thumbnail}`} alt={title} alignSelf="center"/>
         <Box w="100%">
           <Heading size='sm' my={2} px={2} textAlign="center">{title}</Heading>
           <Text size='md' mb={2} textAlign="center">{authors}</Text>
--- bookr/src/components/CardGrid.jsx
+++ bookr/src/components/CardGrid.jsx
@@ -5,17 +5,12 @@ import axios from 'axios'
 
 const CardGrid = ({query}) => {
   const [allBookData, setAllBookData] = useState([])
-  const [allBookTitles, setAllBookTitles] = useState([])
-  const [allBookAuthors, setAllBookAuthors] = useState([])
-  const [allBookImages, setAllBookImages] = useState([])
 
-  const titles = []
-  const authors = []
-  const images = []
-  let cards = []
-  let books = []
-
-  const getBooks = () => {
+  useEffect(() => {
+    //do nothing if the search box is empty
+    if (query==="") {
+      return
+    }
     axios({
       url: 'https://www.googleapis.com/books/v1/volumes',
       params: {
@@ -25,74 +20,34 @@ const CardGrid = ({query}) => {
     })
     .then((response) => {
       const allBooks = response.data.items
-      //console.log(allBooks)
       for (let i = 0; i < allBooks.length; i++) {
-        books.push(allBooks[i].volumeInfo)
-        titles.push(allBooks[i].volumeInfo.title)
-        authors.push(allBooks[i].volumeInfo.authors)
+        if (allBooks[i].volumeInfo.imageLinks == undefined){
+          console.log("IMAGES UNDEFINED")
+          allBooks[i].volumeInfo.imageLinks = {smallThumbnail: "No Image Available", thumbnail: "No Image Available"}
+          continue
+        }
+        if (allBooks[i].volumeInfo.authors === undefined) {
+          allBooks[i].volumeInfo.authors = ["REPLACEMENT TEST"]
+          continue
+        }
+        // If there's more than one, add a comma and a space to each item
+        if (allBooks[i].volumeInfo.authors.length > 1) {
+          for (let i = 0; i < allBooks[i].volumeInfo.authors.length - 1; i++) {
+            allBooks[i].volumeInfo.authors += ", "
+          }
+        }
       }
-
-      // //authorList is an inner array of authors. If there's more than one, add a space to each item
-      // for (let authorList of authors) {
-      //   if (authorList === undefined) {
-      //     authorList = [""]
-      //   }
-      //   if (authorList.length > 1) {
-      //     for (let i = 0; i < authorList.length - 1; i++) {
-      //       authorList[i] += ", "
-      //     }
-      //   }
-      // }
-      
-      // for (let i = 0; i < allBooks.length; i++) {
-      //   if (allBooks[i].volumeInfo.imageLinks === undefined){
-      //     images.push({smallThumbnail: "No Image Available", thumbnail: "No Image Available"})
-      //     continue
-      //   }
-      //   images.push(allBooks[i].volumeInfo.imageLinks)
-      // }
+      for (let book of allBooks) {
+        console.log(book.volumeInfo.authors)
+      }
       setAllBookData(allBooks)
-      // setAllBookTitles(titles)
-      // setAllBookAuthors(authors)
-      // setAllBookImages(images)
     })
     .catch((err) => {
       console.error(err);
     })
-  }
+  }, [query])
   
   console.log(allBookData)
-  useEffect(() => {
-    //do nothing if the search box is empty
-    if (query==="") {
-      return
-    }
-    getBooks()
-    for (let book of allBookData) {
-        if (book.volumeInfo.imageLinks === undefined){
-          book.volumeInfo.imageLinks = {smallThumbnail: "No Image Available", thumbnail: "No Image Available"}
-        }
-        if (book.volumeInfo.authors === undefined) {
-          book.volumeInfo.authors = ""
-        }
-        if (book.volumeInfo.authors.length > 1) {
-          for (let i = 0; i < book.volumeInfo.authors.length - 1; i++) {
-          book.volumeInfo.authors[i] += ", "
-          }
-        }
-      }
-  }, [query, allBookData])
-
-
-  // for (let i = 0; i < allBookData.length; i++) {
-  //   cards.push(
-  //     <SlideFade key={i} in={true}>
-  //       <GridItem className="gridItem" boxShadow="lg" w='100%' h='100%' bg='gray.100' borderRadius={6} >
-  //         <BookCard allBookData={allBookData[i]} title={allBookData[i].title} authors={allBookData[i].authors} image={allBookImages[i]}/>
-  //       </GridItem>
-  //     </SlideFade>
-  //   )
-  // }
 
   return (
       <SimpleGrid minChildWidth={300} spacing={6} m="2rem">
@@ -103,7 +58,7 @@ const CardGrid = ({query}) => {
               bookData={book}
               title={book.volumeInfo.title}
               authors={JSON.stringify(book.volumeInfo.authors)}
-              image={JSON.stringify(book.volumeInfo.imageLinks.thumbNail)}
+              image={book.volumeInfo.imageLinks}
             />
           </GridItem>
         </SlideFade>
