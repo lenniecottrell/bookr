6637daa13508f2898c3d3a2f642810562d424ee2
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-07-17 16:37:06 -0700

    make auth happen on button click only, clean up components

--- bookr/src/App.jsx
+++ bookr/src/App.jsx
@@ -3,14 +3,10 @@ import './styles/App.scss'
 import SearchBar from './components/SearchBar';
 import Nav from './components/Nav';
 import CardGrid from './components/CardGrid';
-import GoogleAuth from './components/GoogleAuth';
-import SignOutButton from './components/SignOutButton';
-import GoogleButton from 'react-google-button'
-import jwt_decode from 'jwt-decode'
 
 
 function App() {
-  //TODO create context for the user state
+  //TODO create context for the user state?
   const [q, setQ] = useState("harry+potter")
   const [user, setUser] = useState({})
   const [token, setToken] = useState("")
@@ -20,17 +16,18 @@ function App() {
     setQ(input.replace(" ", "+"))
   }
 
-  const baseURL = 'https://accounts.google.com/o/oauth2/v2/auth'
-  const client_id = '618793947299-lrlk0trtc9qbej6b6f02vsuv15fh6o6n.apps.googleusercontent.com'
-  const redirect_uri = 'http://localhost:3000'
-  const response_type = 'token'
-  const scope = 'https://www.googleapis.com/auth/books'
+  // const baseURL = 'https://accounts.google.com/o/oauth2/v2/auth'
+  // const client_id = '618793947299-lrlk0trtc9qbej6b6f02vsuv15fh6o6n.apps.googleusercontent.com'
+  // const redirect_uri = 'http://localhost:3000'
+  // const response_type = 'token'
+  // const scope = 'https://www.googleapis.com/auth/books'
   
   const handleAuthorizationResponse = (response) => {
     console.log(response)
+    setToken(response.access_token)
   }
 
-  
+  //Pretty sure I don't need this...
   const handleAuthenticationResponse = (response) => {
     console.log(response)
     //decode the returned credential
@@ -40,12 +37,7 @@ function App() {
     // setUser(userObject)
   }
 
-  // useEffect(()=> {
-  //   requestAccessToken();
-  // }, [])
-    
-    
-  const requestAccessToken = () => {
+  const getAccessToken = () => {
     const client = google.accounts.oauth2.initTokenClient({
       client_id: '618793947299-lrlk0trtc9qbej6b6f02vsuv15fh6o6n.apps.googleusercontent.com',
       scope: 'https://www.googleapis.com/auth/books',
@@ -54,12 +46,9 @@ function App() {
     client.requestAccessToken();
   }
 
-
   return (
     <div className="App">
-      <Nav />
-      <GoogleButton onClick={requestAccessToken()}/>
-      {/* <GoogleAuth /> */}
+      <Nav getAccessToken={getAccessToken}/>
       <SearchBar handleSearchChange={handleSearchChange} value={q} title={"Find A Book"}/>
       <CardGrid query={q}/>
     </div>
--- bookr/src/components/Nav.jsx
+++ bookr/src/components/Nav.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {Link as RouterLink} from 'react-router-dom'
-import GoogleAuth from './GoogleAuth';
+import GoogleButton from 'react-google-button'
 import {
   Flex,
   Spacer,
@@ -9,14 +9,7 @@ import {
   Heading,
 } from '@chakra-ui/react'
 
-const Nav = () => {
-
-  useEffect(() => {
-      google.accounts.id.renderButton(
-      document.querySelector('.signIn'),
-      {theme: 'outline', size: 'large'}
-    )
-  })
+const Nav = ({getAccessToken}) => {
 
   return (
     <Flex className='nav' display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" m={5} px={5}>
@@ -28,7 +21,7 @@ const Nav = () => {
         <Link as={RouterLink} to="/about" mx={5}>About</Link>
         <Link as={RouterLink} to="/account" mx={5}>Account</Link>
       </Container>
-      <GoogleAuth />
+      <GoogleButton onClick={() => getAccessToken()} />
     </Flex>
   )
 }
