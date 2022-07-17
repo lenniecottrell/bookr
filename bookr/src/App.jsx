0271f240a0f20773b2c477767d6084ce1b3f2038
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-07-17 09:46:26 -0700

    finally got the token request working!

--- bookr/src/App.jsx
+++ bookr/src/App.jsx
@@ -5,45 +5,62 @@ import Nav from './components/Nav';
 import CardGrid from './components/CardGrid';
 import GoogleAuth from './components/GoogleAuth';
 import SignOutButton from './components/SignOutButton';
+import GoogleButton from 'react-google-button'
 import jwt_decode from 'jwt-decode'
 
 
 function App() {
-  const [q, setQ] = useState("harry+potter")
   //TODO create context for the user state
+  const [q, setQ] = useState("harry+potter")
   const [user, setUser] = useState({})
+  const [token, setToken] = useState("")
 
-  const handleChange = (e) => {
+  const handleSearchChange = (e) => {
     let input = e.target.value.trim()
     setQ(input.replace(" ", "+"))
   }
 
-  const handleCallbackResponse = (response) => {
-    //decode the returned credential
-    var userObject = jwt_decode(response.credential)
-    //comma instead of plus to display two different data types
-    console.log("UserObject: ", userObject )
-    setUser(userObject)
+  const baseURL = 'https://accounts.google.com/o/oauth2/v2/auth'
+  const client_id = '618793947299-lrlk0trtc9qbej6b6f02vsuv15fh6o6n.apps.googleusercontent.com'
+  const redirect_uri = 'http://localhost:3000'
+  const response_type = 'token'
+  const scope = 'https://www.googleapis.com/auth/books'
+  
+  const handleAuthorizationResponse = (response) => {
+    console.log(response)
   }
 
-  useEffect(()=> {
-    //global google
-    google.accounts.id.initialize({
-      client_id: '618793947299-lrlk0trtc9qbej6b6f02vsuv15fh6o6n.apps.googleusercontent.com',
-      callback: handleCallbackResponse
-    });
+  
+  const handleAuthenticationResponse = (response) => {
+    console.log(response)
+    //decode the returned credential
+    // var userObject = jwt_decode(response.credential)
+    // //comma instead of plus to display two different data types
+    // console.log("UserObject: ", userObject )
+    // setUser(userObject)
+  }
 
-    google.accounts.id.renderButton(
-      document.querySelector('.signIn'),
-      {theme: 'outline', size: 'large'}
-    )
-  }, [])
+  // useEffect(()=> {
+  //   requestAccessToken();
+  // }, [])
+    
+    
+  const requestAccessToken = () => {
+    const client = google.accounts.oauth2.initTokenClient({
+      client_id: '618793947299-lrlk0trtc9qbej6b6f02vsuv15fh6o6n.apps.googleusercontent.com',
+      scope: 'https://www.googleapis.com/auth/books',
+      callback: handleAuthorizationResponse,
+    });
+    client.requestAccessToken();
+  }
+
 
   return (
     <div className="App">
       <Nav />
-      { !user ? <GoogleAuth /> : <SignOutButton setUser={setUser}/>}
-      <SearchBar handleChange={handleChange} value={q} title={"Find A Book"}/>
+      <GoogleButton onClick={requestAccessToken()}/>
+      {/* <GoogleAuth /> */}
+      <SearchBar handleSearchChange={handleSearchChange} value={q} title={"Find A Book"}/>
       <CardGrid query={q}/>
     </div>
   )
--- bookr/src/components/Nav.jsx
+++ bookr/src/components/Nav.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Link as RouterLink} from 'react-router-dom'
+import GoogleAuth from './GoogleAuth';
 import {
   Flex,
   Spacer,
@@ -9,6 +10,14 @@ import {
 } from '@chakra-ui/react'
 
 const Nav = () => {
+
+  useEffect(() => {
+      google.accounts.id.renderButton(
+      document.querySelector('.signIn'),
+      {theme: 'outline', size: 'large'}
+    )
+  })
+
   return (
     <Flex className='nav' display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" m={5} px={5}>
       <Heading size="2xl">Bookr</Heading>
@@ -19,7 +28,7 @@ const Nav = () => {
         <Link as={RouterLink} to="/about" mx={5}>About</Link>
         <Link as={RouterLink} to="/account" mx={5}>Account</Link>
       </Container>
-
+      <GoogleAuth />
     </Flex>
   )
 }
