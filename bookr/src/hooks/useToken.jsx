f29dae35e0dbe25948fa05c74c10572f3417f5f6
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-07-20 06:59:43 -0700

    added reference video for Context

--- bookr/src/hooks/useToken.jsx
+++ bookr/src/hooks/useToken.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState} from 'react'
-
+//reference: https://egghead.io/lessons/react-store-a-token-globally-with-react-context-for-easier-use-anywhere-in-a-next-js-app
 export const TokenContext = createContext(null)
 
 export const TokenProvider = ({ children }) => {
