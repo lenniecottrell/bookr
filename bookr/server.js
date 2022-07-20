90c935a873b83c1d364f205de4c415b232963203
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-07-19 22:29:59 -0700

    create Books API call from the backend

--- bookr/server.js
+++ bookr/server.js
@@ -9,11 +9,26 @@ app.use(cors())
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`)
+  console.log(`~~~~~~~************~~~~~~~~~~~~~~~~~~~~~***********~~~~~~~~~~~~~~~~~~`)
 })
 
 app.route('/to-read')
   .get((req, res) => {
-    console.log("request params: ", req.url, req.query)
+    console.log("request params: ", req.query)
+    const headers = {
+      'Authorization': `Bearer ${req.query.token}`,
+      'Content-Type': 'application/json',
+    }
+    axios.post(
+      `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${req.query.shelfId}/addVolume?volumeId=${req.query.bookId}`,
+      {},
+      {headers: headers}
+    ).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error.response.data)
+    })
+    
     res.send("adding a book to 'To Read'")
   })
 
@@ -28,15 +43,3 @@ app.route('/have-read')
     console.log("request params: ", req.url, req.query)
     res.send("adding a book to 'Have Read'")
   })
-
-// axios.post(
-//   'https://www.googleapis.com/books/v1/mylibrary/bookshelves/2/addVolume',
-//   {
-//     volumeId: bookId,
-//     key: process.env.REACT_APP_API_KEY
-//   }
-// ).then((response) => {
-//   console.log(response);
-// }).catch((error) => {
-//   console.error(error)
-// })
