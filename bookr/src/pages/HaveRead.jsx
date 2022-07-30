b604d1752f6c0b9bcf6035f6ce77552a1dd15b3e
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-07-29 21:25:52 -0700

    rendered books to shelves if books exist on a shelf

--- bookr/src/pages/HaveRead.jsx
+++ bookr/src/pages/HaveRead.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import CardGridShelf from '../components/CardGridShelf'
 import EmptyShelf from '../components/EmptyShelf'
-import {Container} from '@chakra-ui/react'
+import { useToken } from '../hooks/useToken'
+import {Box} from '@chakra-ui/react'
 
 const HaveRead = () => {
+  const [books, setBooks] = useState([])
+  const token = useToken().token
+  //TODO
+    //put an API call to the server that checks for books on the bookshelf
+    //if the user isn't logged in, prompt them to log in with a CTA and a button
+    //if the user is logged in but the shelf is empty, render the EmptyShelf component
+    //if the user is logged in and the shelf has books, render the CardGridShelf
+
+  useEffect(()=>{
+    
+    axios.get(
+      //google shelf ids (https://developers.google.com/books/docs/v1/using#ids):
+      //To read = 2
+      //Reading Now = 3
+      //Have Read = 4
+      'http://localhost:5000/get-shelf', {
+        params: {
+          shelfId: 4,
+          token: token
+        }
+      })
+      .then((response) => {
+        console.log(response)
+        setBooks(response.data) //array of objects
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }, [])
+
   return (
-    <Container textAlign="center">
-      <EmptyShelf />
-    </Container>
+    <Box textAlign="center">
+      {books.length === 0 ? <EmptyShelf /> : <CardGridShelf books={books}/>}
+    </Box>
   )
 }
 
--- bookr/src/pages/ReadingNow.jsx
+++ bookr/src/pages/ReadingNow.jsx
@@ -1,32 +1,40 @@
-import React, {useEffect, useContext} from 'react'
-import { Container, Heading, Box } from '@chakra-ui/react'
+import React, {useState, useEffect} from 'react'
+import { Box } from '@chakra-ui/react'
+import CardGridShelf from '../components/CardGridShelf'
 import EmptyShelf from '../components/EmptyShelf'
 import axios from 'axios'
 import { useToken } from '../hooks/useToken'
 
 const ReadingNow = () => {
-  // const tokenValue = useToken().token
-  // console.log(tokenValue)
+  const [books, setBooks] = useState([])
+  const token = useToken().token
 
-  axios.get(
-    'http://localhost:5000/get-shelf', {
-      params: {
-        shelfId: 2,
-        token: useToken().token
-      }
-    })
-    .then((response) => {
-      console.log(response)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+  useEffect(() => {
 
+    axios.get(
+      //google shelf ids (https://developers.google.com/books/docs/v1/using#ids):
+      //To read = 2
+      //Reading Now = 3
+      //Have Read = 4
+      'http://localhost:5000/get-shelf', {
+        params: {
+          shelfId: 3,
+          token: token
+        }
+      })
+      .then((response) => {
+        console.log(response)
+        setBooks(response.data) //array of objects
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },[])
 
   return (
-    <Container textAlign="center">
-      <EmptyShelf />
-    </Container>
+    <Box textAlign="center">
+      {books.length === 0 ? <EmptyShelf /> : <CardGridShelf books={books}/>}
+    </Box>
   )
 }
 
