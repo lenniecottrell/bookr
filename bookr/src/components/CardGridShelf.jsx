f04f1912018921635afcaba29129c62c2d512cbc
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-08-06 22:13:50 -0700

    remove unnecessary code from BookDetailShelf

--- bookr/src/components/CardGridShelf.jsx
+++ bookr/src/components/CardGridShelf.jsx
@@ -26,27 +26,6 @@ const CardGridShelf = ({
   };
   console.log(books);
 
-  //TODO: this is copied from CardGrid - there's probably a DRY way to do this...
-  //useEffect(() => {
-  // for (let i = 0; i < books.length; i++) {
-  //   if (books[i].volumeInfo.imageLinks == undefined) {
-  //     books[i].volumeInfo.imageLinks = {
-  //       smallThumbnail: "No Image Available",
-  //       thumbnail: "No Image Available",
-  //     };
-  //   }
-  //   if (books[i].volumeInfo.authors === undefined) {
-  //     books[i].volumeInfo.authors = [""];
-  //   }
-  //   // If there's more than one, add a comma and a space to each item
-  //   if (books[i].volumeInfo.authors.length > 1) {
-  //     for (let j = 0; j < books[i].volumeInfo.authors.length - 1; j++) {
-  //       books[i].volumeInfo.authors[j] += ", ";
-  //     }
-  //   }
-  // }
-  //}, []);
-
   return (
     <>
       <SimpleGrid minChildWidth={300} spacing={6} m="2rem">
